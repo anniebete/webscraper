5defe5805af73f8f3100e303e52b8a8dbfb97b89
anniebete <beannie2day@gmail.com>
2020-08-17 21:45:45 -0400

    Test commit

--- webscraper.py
+++ webscraper.py
@@ -77,6 +77,8 @@ a = returnUrls(soup, "US_AFRL_CarrollHS")
 for each in a:
     print(each)
 
+print("test commit")
+
 # run first file
 # this = return_webpage("https://2019.igem.org/Team:US_AFRL_CarrollHS")
 # print(this.text)
