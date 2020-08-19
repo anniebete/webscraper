a25a10c8926a1fddd3cb9355106d49f4f6f9157e
anniebete <beannie2day@gmail.com>
2020-08-19 11:39:01 -0400

    Update

--- webscraper.py
+++ webscraper.py
@@ -8,37 +8,6 @@ import csv
 import string
 from time import sleep
 
-
-# returns list with all urls
-def returnUrls(soup, url):
-    urlList = [url]
-    hyperlinks = soup.find_all("a")
-
-    # use initial url to find base and netloc
-    basePath = urlparse(url).path
-    base = basePath
-    netloc = urlparse(url).netloc
-
-    for hyperlink in hyperlinks:
-        href = hyperlink.get("href")
-        path = urlparse(href).path
-
-        # determine if page is wiki page
-        sections = osp.split(path)
-        if isinstance(sections[0], str):
-            if base in sections[0] and href not in urlList:
-                if " " in href:
-                    href = href.replace(" ", "_")
-                if urlparse(href).netloc == netloc:
-                    urlList.append(href)
-
-                # add netloc to url if not present
-                else:
-                    urlList.append("http://" + netloc + href)
-
-    return urlList
-
-
 # function to return webpage from a url
 def processPage(soup):
     h = ["h1", "h2", "h3", "h4", "h5", "h6"]
@@ -64,45 +33,22 @@ def processPage(soup):
             for child in each.descendants:
                 print(child)
 """
-# for each in content:
 
-
-# function to check if url is part of the team's wiki and if not, to add it to the list of links
-
-
-# import team list using csv library
+# assign all teams list to teamData
 with open('iGEM All Teams.csv', newline='') as file:
     reader = csv.reader(file)
     teamData = list(reader)
 
-# setup for status
-i = 1
-length = len(teamData)
-print("Status:")
+# assign iGEM urls to urls
+with open('All Teams URLs.csv', newline='') as file:
+    reader = csv.reader(file)
+    urls = list(reader)
 
-# add all team page urls to teamData
-for team in teamData:
-    name = team[0]
-    url = team[8]
-    page = ''
+allData = list(zip(teamData, urls))
 
-    while page == '':
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        team.append(returnUrls(soup, url))
-
-        # print updated status
-        print(str(i) + "/" + str(length))
-        i = i + 1
-
-
-# create new csv file to write team data to
-with open('All Teams URLs.csv', 'w', newline='') as newFile:
-    writer = csv.writer(newFile, delimiter=',')
-    for team in teamData:
-        writer.writerow(team[10])
-
-newFile.close()
+"""# prints team list
+for team in allData:
+    print(team)"""
 
 """# prints keys
 print(teamData.keys())
@@ -112,13 +58,17 @@ pd.options.display.width
 pd.set_option('display.width', None)
 print(teamData)"""
 
-"""# test an individual page with returnUrls
-page = requests.get("http://2013.igem.org/Team:Carnegie_Mellon")
-soup = BeautifulSoup(page.content, 'html.parser')
-processPage(soup)
-a = returnUrls(soup, "http://2013.igem.org/Team:Carnegie_Mellon")
+# test an individual page with returnUrls
+i = 1
+for url in allData[1][1]:
+    print("Page #" + str(i))
+    print(url)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    processPage(soup)
+    i = i + 1
 
-for each in a:
+"""for each in a:
     print(each)"""
 
 # run first file
