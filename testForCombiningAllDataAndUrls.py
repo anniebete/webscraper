570d954e1346229fc8a3d761201e91113714e6d3
anniebete <beannie2day@gmail.com>
2020-08-19 00:52:48 -0400

    Create

---
+++ testForCombiningAllDataAndUrls.py
@@ -0,0 +1,19 @@
+import csv
+
+# the goal of this script was to test how combining "TEST - iGEM Teams.cvs" and "TEST - iGEM URLs.cvs"
+# this goal was accomplished
+with open('TEST - iGEM Teams.csv', newline='') as file:
+    reader = csv.reader(file)
+    teamData = list(reader)
+
+with open('TEST - iGEM URLs.csv', newline='') as file:
+    reader = csv.reader(file)
+    urls = list(reader)
+
+allData = list(zip(teamData, urls))
+
+# prints team list
+for team in allData:
+    print(team[0][0])
+    print("\n")
+
